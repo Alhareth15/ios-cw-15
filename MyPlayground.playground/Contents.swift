212cae18bd329113d406d59a508a060e3168f879
Alhareth15 <100311546+Alhareth15@users.noreply.github.com>
2022-03-19 10:45:19 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+struct Student {
+    var Name: String
+    var age: Int
+    var grade: Double
+    var atendence: Bool
+
+}
+var alhareth = Student(Name: "alhareth", age:15, grade:100, atendence: true)
+print(alhareth)
